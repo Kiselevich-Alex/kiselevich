bb22e17ea40c6df3110e082b9c3209f0dc013631
Kiselevich-Alex <54080508+Kiselevich-Alex@users.noreply.github.com>
2019-08-31 15:30:16 +0300

    edit navbar

--- 3/js/index.js
+++ 3/js/index.js
@@ -1,20 +1,45 @@
 $(document).ready(function(){
 	$("#News .btn_more").on("click","button", function (event) {
 		if ( $('.row').hasClass('none') ){
-		 $('.row').removeClass('none');
-		 $('#News .btn_more').addClass('none');
+			$('.row').removeClass('none');
+			$('#News .btn_more').addClass('none');
 		}
 	});
-});
-
-$(document).ready(function(){
 	$(".nav").on("click","a", function (event) {
 		event.preventDefault();
 
 		var id  = $(this).attr('href'),
 
-			top = $(id).offset().top;
+		top = $(id).offset().top;
 		
 		$('body,html').animate({scrollTop: top}, 300);
 	});
+	$(".mob_nav").on("click","a", function (event) {
+		event.preventDefault();
+
+		var id  = $(this).attr('href'),
+
+		top = $(id).offset().top;
+		
+		$('body,html').animate({scrollTop: top}, 300);
+
+		if($('.mob_nav').css('display') == "none"){
+			$('.mob_nav').css('display','block');
+		}
+		else{
+			$('.mob_nav').css('display','none');
+		}
+		
+	});
+	$(".mob_header").on("click","button", function (event) {
+		event.preventDefault();
+
+		if($('.mob_nav').css('display') == "none"){
+			$('.mob_nav').css('display','block');
+		}
+		else{
+			$('.mob_nav').css('display','none');
+		}
+	});
+
 });
